56696dfbf7778bfd03406e935b5535155d556d69
7Ji <pugokushin@gmail.com>
2023-09-02 10:00:47 +0800

    scripts/makepkg_to_pkgs.bash: package directly into pkgs

--- scripts/makepkg_to_pkgs.bash
+++ scripts/makepkg_to_pkgs.bash
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 # 1: path to build dir
-pushd $1 &>/dev/null
-# --holdver as we updated those repos before even deploying the PKGBUILDs
-# --noextract as sources were extracted during need_build()
-PKGEXT=.pkg.tar \
-    makepkg --holdver --noextract --syncdeps --noconfirm -A
-popd &>/dev/null
-# mv for atomic
 buildid="$(<"$1".id)"
 pkgdir=pkgs/"${buildid}"
 tempdir="${pkgdir}".temp
 mkdir "${tempdir}"
-mv "$1"/*.pkg.tar "${tempdir}"/
+PKGDEST="$(readlink -f "${tempdir}")"
+pushd $1 &>/dev/null
+# --holdver as we updated those repos before even deploying the PKGBUILDs
+# --noextract as sources were extracted during need_build()
+PKGEXT=.pkg.tar PKGDEST="${PKGDEST}"  \
+    makepkg --holdver --noextract --syncdeps --noconfirm -A
+popd &>/dev/null
+# mv for atomic
 mv "${tempdir}" "${pkgdir}"
 for pkg in "${pkgdir}"/*; do
     ln -sf ../"${buildid}"/$(basename "${pkg}") pkgs/updated/
