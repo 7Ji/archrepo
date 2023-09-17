48afc328f770291ad95e0fac7b6a929a66d1534e
7Ji <pugokushin@gmail.com>
2023-09-18 00:39:41 +0800

    scripts/full_update.fish: allow dynamic arch, add useful release note

--- scripts/full_update.fish
+++ scripts/full_update.fish
@@ -1,4 +1,10 @@
 # Clean up the repo, re-create the DB and the release
+# #1: arch
+if test -z "$argv[1]"
+    set arch aarch64
+else
+    set arch $argv[1]
+end
 pushd pkgs/latest
 set files *
 popd # pkgs/latest
@@ -16,10 +22,10 @@ pushd local
 repo-add --verify --sign localrepo.db.tar.zst *.pkg.tar.zst &
 popd # local
 wait # repo-add
-sudo rsync --recursive --verbose --copy-links --delete local/ /srv/http/localrepo/aarch64 &
+sudo rsync --recursive --verbose --copy-links --delete local/ /srv/http/localrepo/$arch &
 pushd github
-gh release delete --yes aarch64
-gh release create aarch64 --notes '' --latest 7Ji.{db,files}{,.sig} *.pkg.tar.zst{,.sig}
+gh release delete --yes $arch
+gh release create $arch --title $arch --notes "Last full update at $(date)" --latest 7Ji.{db,files}{,.sig} *.pkg.tar.zst{,.sig}
 popd # github
 popd # releases
 wait # rsync
