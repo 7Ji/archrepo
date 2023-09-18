86724eba647374573afb45e0e585df78de07a3ac
7Ji <pugokushin@gmail.com>
2023-09-18 11:32:49 +0800

    scripts/partial_update.fish: add

---
+++ scripts/partial_update.fish
@@ -0,0 +1,45 @@
+# Update packages on demand, re-create the DB and the release
+# #1: arch
+if test -z "$argv[1]"
+    set arch aarch64
+else
+    set arch $argv[1]
+end
+pushd pkgs/updated
+set pkgs *.pkg.tar.zst
+set sigs *.pkg.tar.zst.sig
+if test (count $pkgs) -eq 0
+    exit 0
+end
+if test (count $pkgs) -ne (count $sigs)
+    exit 1
+end
+popd # pkgs/updated
+set files $pkgs $sigs
+pushd releases
+for file in $files
+    ln -sf ../../pkgs/latest/$file local/$file
+    ln -sf ../../pkgs/latest/$file local/(string replace ':' '.' $file)
+end
+pushd github
+repo-add --verify --sign 7Ji.db.tar.zst (string replace ':' '.' $pkgs) &
+popd # github
+pushd local
+repo-add --verify --sign localrepo.db.tar.zst $pkgs &
+popd # local
+wait
+sudo rsync --recursive --verbose --copy-links --delete local/ /srv/http/localrepo/$arch &
+pushd github
+set temp_assets (mktemp)
+gh release view $arch | sed -n 's/^asset:	\(.\+\)$/\1/p' > $temp_assets
+set gh_files (string replace ':' '.' $files)
+set --append gh_files 7Ji.{db,files}{,.sig}
+for file in $gh_files
+    if grep "^$file\$" $temp_assets
+        gh release delete-asset $arch $file --yes
+    end
+end
+rm -f $temp_assets
+gh release upload $arch $gh_files
+popd # github
+popd # releases
