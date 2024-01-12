de6d60a5ed65ef88b3cc55275a1d5477e32c89a5
Guoxin Pu <pugokushin@gmail.com>
2024-01-12 13:01:42 +0800

    full update: remove pkgs not in db before upload

--- scripts/full_update.fish
+++ scripts/full_update.fish
@@ -15,6 +15,18 @@ for file in $files
     ln -sf (string replace '../' '../pkgs/' $file) (string split --right --max 1 --fields 2 '/' $file | string replace ':' '.')
 end
 repo-add --verify --sign 7Ji.db.tar.zst *.pkg.tar.zst
+# Try to remove files not in repo list
+set db (mktemp -d)
+tar -xvf 7Ji.db -C $db
+set list (mktemp)
+sed -n '/%FILENAME%/{n;p;}' $db/*/desc | sort | uniq > $list
+rm -rf $db
+for pkg in *.pkg.tar.zst
+    if ! grep -q "^$pkg\$" $list
+        rm -f $pkg{,.sig}
+    end
+end
+rm -f $list
 rsync --archive --recursive --verbose --copy-links --delete ./ /srv/http/repo/7Ji/$arch &
 gh release delete --yes $arch
 gh release create $arch --title $arch --notes "Last full update at $(date)" --latest 7Ji.{db,files}{,.sig} *.pkg.tar.zst{,.sig}
