615d22daa8f34de06db19f262a0d50860d011d13
Guoxin Pu <pugokushin@gmail.com>
2024-07-05 15:02:39 +0800

    script: re-implement build daemon

--- scripts/common.sh
+++ scripts/common.sh
@@ -55,7 +55,53 @@ default_or() { #1: value, #2: default
 
 build_daemon() {
     assert_declared repo arch config rsync_parent args_builder
+    local commit_remote commit_local idle integ_self args_arb
 
+    args_arb=(--noclean "${args_builder[@]}" "${config}")
+    integ_self=$(sha256sum $(readlink -f -- "$0")) # Both checksum and path
+    enable sleep
+    idle=60
+    while true; do
+        commit_remote=$(git ls-remote origin master)
+        commit_remote="${commit_remote::40}"
+        commit_local=$(git rev-parse master)
+        if [[ "${commit_remote}" == "${commit_local}" ]]; then
+            if (( "${idle}" < 60 )); then
+                let idle++
+                sleep 60
+                continue
+            fi
+        else
+            warn "Updating ${commit_local} -> ${commit_remote}"
+            git fetch origin '+refs/heads/master:refs/remotes/origin/master'
+            git reset --hard origin/master
+            if [[ $(sha256sum $(readlink -f -- "$0")) != "${integ_self}"* ]]; then
+                warn 'Daemon script updated, exit to let outer supervisor decide whether to continue'
+                exit 0
+            fi
+        fi
+        idle=0
+        info "Starting builder with arguments: ${args_arb[@]}"
+        if sudo ./arb "${args_arb[@]}"; then
+            info 'Successfullly built, doing full update'
+            if ! full_update; then
+                error 'Full update failed, maintainer attention needed'
+                exit 1
+            fi
+            info 'Full update successful'
+        else
+            error 'Failed to build, checking if partial update needed'
+            if [[ "$(ls -A pkgs/updated)" ]]; then
+                warn 'Found built packages although build was failed, doing partial update'
+                if ! partial_update; then
+                    error 'Failed to partial update after a failed build with built packages, refuse to continue'
+                    exit 1
+                fi
+                info 'Partial update successful, next build will be triggered immediately'
+                idle=60
+            fi
+        fi
+    done
 }
 
 argparse_build_daemon() {
@@ -64,6 +110,7 @@ argparse_build_daemon() {
     config=config.yaml
     rsync_parent='/srv/http/repo'
     args_builder=()
+
     while (( $# > 0 )); do
         case "$1" in
             --repo)
@@ -93,14 +140,22 @@ argparse_build_daemon() {
 
 cli_build_daemon() { 
     local repo arch config rsync_parent args_builder
+
     argparse_build_daemon "$@"
     build_daemon
+}
+
+remote_update() {
+    assert_declared rsync_parent repo arch
+
+    rsync --recursive --copy-links --update --delete --verbose releases/ "${rsync_parent}/${repo}/${arch}/"
 }
 
 shopt -s extglob # Because Bash checks glob syntax in function definition
 
 full_update() {
     assert_declared repo arch rsync_parent
+
     local link_target link_path file_name db
 
     rm -rf releases
@@ -120,6 +175,7 @@ full_update() {
 
 partial_update() {
     assert_declared repo arch rsync_parent
+
     local link_target link_path file_name db pkgs_to_add=() dir_db list_pkgs_to_keep
 
     for link_path in pkgs/updated/*; do
@@ -154,6 +210,7 @@ argparse_update() {
     repo=7Ji
     arch=aarch64
     rsync_parent='/srv/http/repo'
+
     while (( $# > 0 )); do
         case "$1" in
             --repo)
@@ -175,12 +232,14 @@ argparse_update() {
 
 cli_full_update() {
     local arch= rsync_parent=
+
     argparse_update "$@"
     full_update
 }
 
 cli_partial_update() {
     local arch= rsync_parent=
+
     argparse_update "$@"
     partial_update
 }
